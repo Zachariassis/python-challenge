2a2b1952651a979f228c7121e6fb7a3408b0350d
Zachariassis <Zachariassis@gmail.com>
2020-08-12 12:35:05 -0500

    Completed PyPoll script

---
+++ PyPoll/main.py
@@ -0,0 +1,53 @@
+import os
+import csv
+
+csvpath=os.path.join('C:/Users/Zacha/Documents','python-challenge','PyPoll','election_data.csv')
+Candidates=["empty"]
+Results=[10]
+Percent=[]
+num=0
+
+with open(csvpath) as csvfile:
+
+    csvreader=csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        num=num+1
+        test=row[2] in Candidates
+        if test == True:
+            Results[Candidates.index(row[2])]=Results[Candidates.index(row[2])]+1
+        else:
+            Candidates.append(row[2])
+            Results.append(1)
+            if "empty" in Candidates:
+                Candidates.remove("empty")
+                Results.remove(10)
+
+max=0
+for i in range(len(Candidates)):
+    Percent.append(0)
+    Percent[i]="{:.3%}".format(Results[i]/num)
+    if Results[i]>max:
+        max=Results[i]
+        Winner=Candidates[i]
+
+print("Election Results")
+print("---------------------")
+print(f"Total Votes: {num}")
+print("---------------------")
+for i in range(len(Candidates)):
+    print(f"{Candidates[i]}: {Percent[i]} {Results[i]}")
+print("---------------------")
+print(f"Winner: {Winner}")
+print("---------------------")
+
+output_path = os.path.join('C:/Users/Zacha/Documents','python-challenge','PyPoll','Election_Results.csv')
+
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['Total Votes', num])
+    for i in range(len(Candidates)):
+        csvwriter.writerow([Candidates[i],Percent[i],Results[i]])
+    csvwriter.writerow(['Winner',Winner])
